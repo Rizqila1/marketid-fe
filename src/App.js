ed1e3f6e055cc4e95f08163905a7e1b057bd3fc2
Rizqila1 <ikilatr@gmail.com>
2023-07-13 18:58:57 +0700

    add store loading

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import Loading from "./components/c_Loading";
+import { useSelector } from "react-redux";
+
 import "./assets/css/app.css";
 import "react-toastify/dist/ReactToastify.css";
-import Loading from "./components/c_Loading";
 import "./assets/css/loading.css";
 
 function App() {
+  const { isLoading } = useSelector((state) => state.loading);
   return (
     <>
-      <Loading />
+      {isLoading && <Loading />}
       <ToastContainer />
       <main className="app-main">
         <Outlet />
