d96bdd4d0d418ae26a111ae0d2a06c241f9cdbfd
Rizqila1 <ikilatr@gmail.com>
2023-07-18 16:54:58 +0700

    improve styling

--- src/components/Products/Pro_Skeleton.jsx
+++ src/components/Products/Pro_Skeleton.jsx
@@ -1,10 +1,10 @@
 import { Card, Placeholder } from "react-bootstrap";
-import defaultImage from "../../assets/images/default-image.jpg";
+import noImage from "../../assets/images/no-image.jpg";
 
 export default function SkeletonCard() {
   return (
     <Card>
-      <Card.Img variant="top" src={defaultImage} />
+      <Card.Img variant="top" src={noImage} />
       <Card.Body>
         <Placeholder as={Card.Text} animation="glow">
           <Placeholder xs={12} />
