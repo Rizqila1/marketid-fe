8c355d04abb1fd10727cef595d7334dcccc6722a
Rizqila1 <ikilatr@gmail.com>
2023-07-18 17:06:32 +0700

    improve styling

---
+++ src/components/Products/Pro_NotFound.jsx
@@ -0,0 +1,21 @@
+import { Image } from "react-bootstrap";
+import Icon from "../../assets/images/no-data.png";
+import "../../assets/css/product-page.css";
+
+export default function ProductNotFound() {
+  return (
+    <>
+      <div className="min-vh-100 d-flex flex-column justify-content-center align-items-center">
+        <h1 className="text-dark heading__1 opacity-75">
+          {"Product Not Found :("}
+        </h1>
+        <br />
+        <Image
+          className="display_notfound mb-5 pb-5"
+          src={Icon}
+          alt="no-data"
+        />
+      </div>
+    </>
+  );
+}
