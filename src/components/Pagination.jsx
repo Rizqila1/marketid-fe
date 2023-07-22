3c35610a31aee4000dc6ec75bdba19f7e3554348
Rizqila1 <ikilatr@gmail.com>
2023-07-23 00:25:48 +0700

    feat: create customize loader

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,7 +1,9 @@
 import { useDispatch, useSelector } from "react-redux";
 import { PaginationControl } from "react-bootstrap-pagination-control";
 import { Row, Form } from "react-bootstrap";
+
 import ProductNotFound from "./Products/Pro_NotFound";
+import Loading2 from "./Loading2";
 
 export default function ComponentPagination(props) {
   const storeParamsProduct = useSelector((state) => state.product);
@@ -18,41 +20,45 @@ export default function ComponentPagination(props) {
     dispatch({ type: "ACTION_PAGE", value: page });
     props.setPagination({ ...props.pagination, page: page });
   }
+
+  const customizeLoader = () => {
+    if (props.loading === true) return <Loading2 />;
+    if (!props.data.length) return <ProductNotFound message={props.message} />;
+  };
+
   return (
     <>
-      {props.data.length ? (
-        <Row>
-          <div className="d-flex justify-content-end align-items-center">
-            <p className="paragraph__3 mb-3">Per Page</p>
-            <div className="mb-3 mx-3">
-              <Form.Select
-                value={storeParamsProduct.per_page}
-                onChange={handleChange}
-                style={{ width: "5rem" }}
-              >
-                <option value="10">10</option>
-                <option value="15">15</option>
-                <option value="20">20</option>
-                <option value="25">25</option>
-              </Form.Select>
-            </div>
+      {customizeLoader()}
 
-            <div>
-              <PaginationControl
-                page={props.pagination.page}
-                total={props.pagination.total}
-                limit={props.pagination.per_page}
-                last={true}
-                ellipsis={1}
-                between={4}
-                changePage={handlePagination}
-              ></PaginationControl>
-            </div>
+      <Row>
+        <div className="d-flex justify-content-end align-items-center">
+          <p className="paragraph__3 mb-3">Per Page</p>
+          <div className="mb-3 mx-3">
+            <Form.Select
+              value={storeParamsProduct.per_page}
+              onChange={handleChange}
+              style={{ width: "5rem" }}
+            >
+              <option value="10">10</option>
+              <option value="15">15</option>
+              <option value="20">20</option>
+              <option value="25">25</option>
+            </Form.Select>
           </div>
-        </Row>
-      ) : (
-        <ProductNotFound message={props.message} />
-      )}
+
+          <div>
+            <PaginationControl
+              page={props.pagination.page}
+              total={props.pagination.total}
+              limit={props.pagination.per_page}
+              last={true}
+              ellipsis={1}
+              between={4}
+              changePage={handlePagination}
+            ></PaginationControl>
+          </div>
+        </div>
+      </Row>
     </>
   );
 }
