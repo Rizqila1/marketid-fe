1ca6284efbe69cb9532ffc076b6bfee7a05655da
Rizqila1 <ikilatr@gmail.com>
2023-07-23 00:22:54 +0700

    feat: add new loader

---
+++ src/components/Loading2.jsx
@@ -0,0 +1,11 @@
+export default function Loading2() {
+  return (
+    <div className="loader__2 mt-3">
+      <div className="circle_loader_2"></div>
+      <div className="circle_loader_2"></div>
+      <div className="circle_loader_2"></div>
+      <div className="circle_loader_2"></div>
+      <div className="circle_loader_2"></div>
+    </div>
+  );
+}
