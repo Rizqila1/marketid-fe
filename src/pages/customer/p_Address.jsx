207989da6d143c9c051fafaae1d1e857a128f26a
Rizqila1 <ikilatr@gmail.com>
2023-07-29 12:24:29 +0700

    feat: pairing address api

--- src/pages/customer/p_Address.jsx
+++ src/pages/customer/p_Address.jsx
@@ -1,6 +1,29 @@
+import { useEffect, useState } from "react";
 import { Breadcrumb, Row, Col, ListGroup, Button } from "react-bootstrap";
+import { axiosInstance as axios } from "../../config/httpsAxios";
+import { useDispatch } from "react-redux";
 
 export default function AddressPage() {
+  const [data, setData] = useState([]);
+
+  const dispatch = useDispatch();
+  let number = 1;
+
+  useEffect(() => {
+    dispatch({ type: "SET_LOADING", value: true });
+    axios
+      .get("/address/list")
+      .then((response) => {
+        setData(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        dispatch({ type: "SET_LOADING", value: false });
+      });
+  }, [dispatch]);
+
   return (
     <>
       <Breadcrumb className="mt-5">
@@ -34,40 +57,48 @@ export default function AddressPage() {
             </div>
 
             <Row className="border_color_brighter d-flex justify-content-between p-2">
-              <Col xs={4} className="d-flex justify-content-between">
+              <Col lg={2} className="d-flex justify-content-between">
                 <h5 className="subheading__2 mx-3 m-0">ID</h5>
-                <h5 className="subheading__2 mx-2 m-0">Name</h5>
+              </Col>
+              <Col lg={6} className="d-flex justify-content-between">
+                <h5 className="subheading__2 m-0">Name</h5>
               </Col>
               <Col
-                xs={6}
+                lg={4}
                 className="d-flex justify-content-end align-items-center"
               >
                 <h5 className="subheading__2 mx-3 m-0">Actions</h5>
               </Col>
             </Row>
 
-            <Row className="border_color_brighter p-2">
-              <Col xs={4} className="d-flex justify-content-between">
-                <h5 className="subheading__2 mt-1 mx-3 m-0">1</h5>
-                <h5 className="subheading__2 mt-1 m-0">Current</h5>
-              </Col>
-              <Col
-                xs={8}
-                className="d-flex justify-content-end align-items-center"
-              >
-                <Button
-                  className="mx-2"
-                  variant="primary"
-                  href="/marketid/address/edit"
-                  style={{ width: "6.25rem" }}
+            {data.map((element, i) => (
+              <Row key={`${i}-address`} className="border_color_brighter p-2">
+                <Col lg={2} className="d-flex justify-content-between">
+                  <h5 className="subheading__2 mt-1 mx-3 m-0">{number++}</h5>
+                </Col>
+                <Col lg={6}>
+                  <h5 className="subheading__2 mt-1 m-0 text-capitalize">
+                    {element.name}
+                  </h5>
+                </Col>
+                <Col
+                  lg={4}
+                  className="d-flex justify-content-end align-items-center"
                 >
-                  Edit
-                </Button>
-                <Button variant="danger" style={{ width: "6.25rem" }}>
-                  Delete
-                </Button>
-              </Col>
-            </Row>
+                  <Button
+                    className="mx-2"
+                    variant="primary"
+                    href="/marketid/address/edit"
+                    style={{ width: "6.25rem" }}
+                  >
+                    Edit
+                  </Button>
+                  <Button variant="danger" style={{ width: "6.25rem" }}>
+                    Delete
+                  </Button>
+                </Col>
+              </Row>
+            ))}
           </section>
         </Col>
       </Row>
