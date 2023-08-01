a09ce961c2e63805833a7c97acec1a2f72028104
Rizqila1 <ikilatr@gmail.com>
2023-08-01 19:30:36 +0700

    feat: fix styling

--- src/components/Products/Pro_NotFound.jsx
+++ src/components/Products/Pro_NotFound.jsx
@@ -5,7 +5,7 @@ import "../../assets/css/product-page.css";
 export default function ProductNotFound(props) {
   return (
     <>
-      <div className="min-vh-100 d-flex flex-column justify-content-center align-items-center">
+      <div className="d-flex flex-column justify-content-center align-items-center py-5 my-5">
         <h1 className="text-dark heading__1 opacity-75">{props.message}</h1>
         <br />
         <Image
