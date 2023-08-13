6a30d489b6d02c7048ed18c5ea6e5c80f23ebbd6
Rizqila1 <ikilatr@gmail.com>
2023-08-13 17:39:16 +0700

    feat: handle auth admin

--- src/layouts/Authorization.jsx
+++ src/layouts/Authorization.jsx
@@ -1,11 +1,20 @@
 import { Navigate, useLocation } from "react-router-dom";
 
 const LayoutAuthorization = ({ auth, children }) => {
-  const { token } = auth;
+  const { token, user } = auth;
   const location = useLocation();
   const isPage = ["/marketid/login", "/marketid/register"].includes(
     location.pathname
   );
+
+  // FOR ROLE ADMIN
+  if (user.role && ["admin"].includes(user.role.name)) {
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+
+    if (isPage) return children;
+    return <Navigate to="/marketid/login" replace />;
+  }
 
   if (!token) {
     if (isPage) return children;
