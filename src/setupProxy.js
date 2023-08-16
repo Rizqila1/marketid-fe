38972c8d1aa8419f14bb02e0bef07a1ff49dbb63
Rizqila1 <ikilatr@gmail.com>
2023-08-16 16:02:30 +0700

    feat: setup proxy indonesia regions for address

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-const baseURL = {
+const proxybaseURL = {
   target: `https://market-id.vercel.app/api/v1`,
   changeOrigin: true,
   pathRewrite: {
@@ -8,6 +8,15 @@ const baseURL = {
   },
 };
 
+const proxyIndonesiaRegionAPI = {
+  target: `http://www.emsifa.com/api-wilayah-indonesia/api`,
+  changeOrigin: true,
+  pathRewrite: {
+    "^/api-region": "",
+  },
+};
+
 module.exports = function (app) {
-  app.use("/api", createProxyMiddleware(baseURL));
+  app.use("/api", createProxyMiddleware(proxybaseURL));
+  app.use("/api-region", createProxyMiddleware(proxyIndonesiaRegionAPI));
 };
