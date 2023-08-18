ddb427671317ef029fe31d75e5a9877be9953f64
Rizqila1 <ikilatr@gmail.com>
2023-08-19 00:22:17 +0700

    feat: using json for page about

--- src/pages/p_About.jsx
+++ src/pages/p_About.jsx
@@ -1,8 +1,16 @@
-export default function About() {
+import content from "../content/eng/pages_about.json";
 
+export default function About() {
   return (
-    <div>
-      <h1>About</h1>
+    <div className="d-flex flex-column justify-content-center align-items-center min-vh-100">
+      <h1 className="heading__1">{content.marketid_company_title}</h1>
+      <h3 className="subheading__2">{content.marketid_company} </h3>
+      <h2 className="heading__2">{content.marketid_founding_date_title}</h2>
+      <h3 className="subheading__2">{content.marketid_date_founding}</h3>
+      <h2 className="heading__2">{content.marketid_location_title}</h2>
+      <h3 className="subheading__2">{content.marketid_location}</h3>
+      <h2 className="heading__2">{content.marketid_mission_title}</h2>
+      <h3 className="subheading__2 text-center">{content.marketid_mission}</h3>
     </div>
-  )
+  );
 }
