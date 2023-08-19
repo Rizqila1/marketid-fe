52ea59c0e7dcda590fa59adf560cb49200e7b38e
Rizqila1 <ikilatr@gmail.com>
2023-08-20 04:31:12 +0700

    feat: improve page not found

--- src/pages/p_NotFound.jsx
+++ src/pages/p_NotFound.jsx
@@ -1,8 +1,13 @@
-export default function NotFound() {
+import { Button } from "react-bootstrap";
 
+export default function NotFound() {
   return (
-    <div>
-      <h1>404 | Not Found</h1>
+    <div className="d-flex flex-column justify-content-center align-items-center min-vh-100">
+      <h1 className="heading__1">404 | Not Found</h1>
+      <h1 className="heading__3 mt-3">Into Market Indonesia</h1>
+      <Button href="marketid" variant="primary" className="fw-semibold mt-1">
+        Click Here
+      </Button>
     </div>
-  )
+  );
 }
